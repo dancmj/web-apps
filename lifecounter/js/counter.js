3ff586cd182a5bdcbf65a60ed7ead09a29ac1190
dancmj <daniel.c82@gmail.com>
2015-02-23 11:16:29 -0700

    Updated touch settings

--- lifecounter/js/counter.js
+++ lifecounter/js/counter.js
@@ -1,18 +1,19 @@
 $(function() {
-        $("body").click(function(e) {
-          switch(e.target.id){
-            case("PlayerOneBtnPlus"):
-              e.shiftKey ? document.getElementById("PlayerOneLife").innerHTML = parseInt(document.getElementById("PlayerOneLife").innerHTML) + 5 : document.getElementById("PlayerOneLife").innerHTML++;
-              break;
-            case("PlayerOneBtnMinus"):
-              e.shiftKey ? document.getElementById("PlayerOneLife").innerHTML = parseInt(document.getElementById("PlayerOneLife").innerHTML) - 5 : document.getElementById("PlayerOneLife").innerHTML--;
-              break;
-            case("PlayerTwoBtnPlus"):
-              e.shiftKey ? document.getElementById("PlayerTwoLife").innerHTML = parseInt(document.getElementById("PlayerTwoLife").innerHTML) + 5 : document.getElementById("PlayerTwoLife").innerHTML++;
-              break;
-            case("PlayerTwoBtnMinus"):
-              e.shiftKey ? document.getElementById("PlayerTwoLife").innerHTML = parseInt(document.getElementById("PlayerTwoLife").innerHTML) - 5 : document.getElementById("PlayerTwoLife").innerHTML--;
-              break;
-          }
+  var isMobile = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) || false;
+  $("body").on(isMobile ? 'touchend' : 'click',function(e) {
+    switch(e.target.id){
+        case("PlayerOneBtnPlus"):
+          e.shiftKey ? document.getElementById("PlayerOneLife").innerHTML = parseInt(document.getElementById("PlayerOneLife").innerHTML) + 5 : document.getElementById("PlayerOneLife").innerHTML++;
+          break;
+        case("PlayerOneBtnMinus"):
+          e.shiftKey ? document.getElementById("PlayerOneLife").innerHTML = parseInt(document.getElementById("PlayerOneLife").innerHTML) - 5 : document.getElementById("PlayerOneLife").innerHTML--;
+          break;
+        case("PlayerTwoBtnPlus"):
+          e.shiftKey ? document.getElementById("PlayerTwoLife").innerHTML = parseInt(document.getElementById("PlayerTwoLife").innerHTML) + 5 : document.getElementById("PlayerTwoLife").innerHTML++;
+          break;
+        case("PlayerTwoBtnMinus"):
+          e.shiftKey ? document.getElementById("PlayerTwoLife").innerHTML = parseInt(document.getElementById("PlayerTwoLife").innerHTML) - 5 : document.getElementById("PlayerTwoLife").innerHTML--;
+          break;
+      }
     });
 })
